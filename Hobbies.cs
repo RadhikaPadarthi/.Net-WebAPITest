50d066fdec6fc2f03f2c63360c961970e506a8ff
RadhikaPadarthi <radhika.padarthi88@gmail.com>
2023-05-11 13:46:26 -0500

    Add files via upload

---
+++ Hobbies.cs
@@ -0,0 +1,6 @@
+﻿namespace NLBCodingTest
+{
+    public class Hobbies
+    {
+    }
+}
---
+++ StudentInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace NLBCodingTest
+{
+    public class StudentInfo
+    {
+        public string? Name { get; set; }
+
+        public string? Age { get; set; }
+
+        public List<String> Hobbies { get; set; } = new List<String>();
+    }
+}
